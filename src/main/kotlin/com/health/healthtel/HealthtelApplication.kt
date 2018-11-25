f5683959ae5977a4a85c0b48800a5a14b55f6903
oleksandr-berkovskyi-exa <oleksandr.berkovskyi@exabeam.com>
2018-11-25 20:23:31 +0200

    added swagger and save client

--- src/main/kotlin/com/health/healthtel/HealthtelApplication.kt
+++ src/main/kotlin/com/health/healthtel/HealthtelApplication.kt
@@ -2,10 +2,27 @@ package com.health.healthtel
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
+@EnableSwagger2
 @SpringBootApplication
 class HealthtelApplication
 
     fun main(args: Array<String>) {
         runApplication<HealthtelApplication>(*args)
     }
+
+    @Bean
+    fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+    }
+
