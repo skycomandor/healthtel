b8c886a73c48a005445658d1c9b8c4abae7a419f
oleksandr-berkovskyi-exa <oleksandr.berkovskyi@exabeam.com>
2019-02-10 11:30:02 +0200

    allow cross request

--- src/main/kotlin/com/health/healthtel/entities/Client.kt
+++ src/main/kotlin/com/health/healthtel/entities/Client.kt
@@ -1,5 +1,7 @@
 package com.health.healthtel.entities
 
+import org.hibernate.annotations.NotFound
+import org.hibernate.annotations.NotFoundAction
 import javax.persistence.*
 
 @Entity(name = "clients")
@@ -22,6 +24,8 @@ data class Client(
         val birthyear: Int? = null,
         @Column(nullable = true)
         val address: String? = null,
-        @Column(name = "doctor_id")
-        val doctorID: Int? = null
+        @ManyToOne(fetch = FetchType.EAGER)
+        @NotFound(action = NotFoundAction.IGNORE)
+        @JoinColumn(name = "doctor_id", referencedColumnName="id")
+        val doctor: Employees? = null
 )
