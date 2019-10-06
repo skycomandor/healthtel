55b6a692a4f0c276902307974eef3249b27f65e5
oberkovskyi <oleksandr.berkovskyi@zalando.de>
2019-10-06 23:11:13 +0200

    fix migration

--- src/main/resources/db/migration/V00__init.sql
+++ src/main/resources/db/migration/V00__init.sql
@@ -81,35 +81,11 @@ INSERT INTO `employees` VALUES (1,'Andrey','Telnov','','m','admin','minaht','eKs
 UNLOCK TABLES;
 
 --
--- Table structure for table `flyway_schema_history`
---
-
-DROP TABLE IF EXISTS `flyway_schema_history`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `flyway_schema_history` (
-  `installed_rank` int(11) NOT NULL,
-  `version` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `description` varchar(200) COLLATE utf8mb4_general_ci NOT NULL,
-  `type` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
-  `script` varchar(1000) COLLATE utf8mb4_general_ci NOT NULL,
-  `checksum` int(11) DEFAULT NULL,
-  `installed_by` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `installed_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `execution_time` int(11) NOT NULL,
-  `success` tinyint(1) NOT NULL,
-  PRIMARY KEY (`installed_rank`),
-  KEY `flyway_schema_history_s_idx` (`success`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Dumping data for table `flyway_schema_history`
 --
 
 LOCK TABLES `flyway_schema_history` WRITE;
 /*!40000 ALTER TABLE `flyway_schema_history` DISABLE KEYS */;
-INSERT INTO `flyway_schema_history` VALUES (1,'00','start','SQL','V00__start.sql',281537155,'root','2019-10-04 15:00:21',10,1);
 /*!40000 ALTER TABLE `flyway_schema_history` ENABLE KEYS */;
 UNLOCK TABLES;
 
